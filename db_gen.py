0684b5a34d55940855c9903a65c8fc57a7eceb09
Vipul Ujawane <vipul999ujawane@gmail.com>
2019-03-16 02:39:55 +0530

    DataBase Generating Script Ready

---
+++ db_gen.py
@@ -0,0 +1,26 @@
+from elasticsearch import Elasticsearch,exceptions
+import ast
+import sys
+
+def connect(filename, indexname, doc_type):
+    ES_HOST   = {"host":"localhost","port":9200}
+    es = Elasticsearch(hosts=[ES_HOST])
+    INDEX_NAME = indexname
+    try:
+        response = es.indices.create(index=INDEX_NAME)
+    except exceptions.RequestError:
+        print("Index Already Created")
+        return
+    print(response)
+    db = open(filename)
+    lists = db.readlines()
+    i=1
+    for line in lists:
+        body = ast.literal_eval(line[2:-2])
+        resp = es.index(index=INDEX_NAME,doc_type=doc_type,body=body,id=i)
+        i=i+1
+        print(resp)
+
+
+if __name__ == "__main__":
+    connect(sys.argv[1],sys.argv[2],sys.argv[3])
