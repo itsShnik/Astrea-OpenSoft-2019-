710a72376e4c620802c3f3cb7dc8c06d04449926
Vipul Ujawane <vipul999ujawane@gmail.com>
2019-03-19 22:33:09 +0530

    Added More Filters

--- server.py
+++ server.py
@@ -17,14 +17,24 @@ def welcome():
 @app.route("/search")
 def search():
     query=request.args.get('q')
+    judge=request.args.get('judge')
+    category=request.args.get('category')
+    acts=request.args.get('acts')
     s = Search(using=client)
-    q_base=Q('multi_match',query=query,fuzziness="1",prefix_length=3)
     should =[]
-    should.append(q_base)
-    judge = request.args.get('judge')
+    if(query is not None):
+        q_base=Q('multi_match',query=query,fuzziness="1",prefix_length=3)
+        should.append(q_base)
+        judge = request.args.get('judge')
     if(judge is not None):
         q_judge = Q('multi_match',query=judge,fields=['Judge'])
         should.append(q_judge)
+    if(acts is not None):
+        q_acts = Q('multi_match',query=acts,fields=['Judge'])
+        should.append(q_acts)
+    if(category is not None):
+        q_category = Q('multi_match',query=category,fields=['Judge'])
+        should.append(q_category)
     q = Q('bool',should=should,minimum_should_match=len(should))
     s=s.query(q)
     count=s.count()
